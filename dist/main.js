4c6792702de5ee3ca9947f603245e2125a082cbe
Romeo <romeovs@gmail.com>
2020-03-06 11:22:33 +0000

    Simplify file fetching loop

--- dist/main.js
+++ dist/main.js
@@ -22922,30 +22922,22 @@ async function changedFiles(github, repo, pr, count) {
 	const files = [];
 
 	for (const page = 0; page * perPage < count; page++) {
-		await Promise.all(
-			commits.map(async function(commit) {
-				if (!commit.distinct) {
-					return
-				}
+		const response = await github.pulls.listFiles({
+			...repo,
+			pull_number: pr,
+			page,
+			per_page: perPage,
+		});
 
-				const response = await github.pulls.listFiles({
-					...repo,
-					pull_number: pr,
-					page,
-					per_page: perPage,
-				});
+		if (!response || !response.data) {
+			return
+		}
 
-				if (!response || !response.data) {
-					return
-				}
-
-				for (const file of result.data) {
-					if (file.status === "added" || file.status === "modified") {
-						files.push(file.filename);
-					}
-				}
-			}),
-		);
+		for (const file of result.data) {
+			if (file.status === "added" || file.status === "modified" || file.status === "renamed") {
+				files.push(file.filename);
+			}
+		}
 	}
 
 	return files
