dd4e151daef92cadb7d3171c6585aa14867501c3
mszlgr <mszlagor@remitly.com>
2020-10-28 11:39:15 +0100

    Update src

--- src/comment.js
+++ src/comment.js
@@ -6,9 +6,7 @@ import { tabulate } from "./tabulate"
 export function comment (lcov, options) {
 	return fragment(
 		`Coverage after merging ${b(options.head)} into ${b(options.base)}`,
-		table(tbody(tr(th(percentage(lcov).toFixed(2), "%")))),
-		"\n\n",
-		details(summary("Coverage Report"), tabulate(lcov, options)),
+		table(tbody(tr(th(percentage(lcov).toFixed(2), "%"))))
 	)
 }
 
